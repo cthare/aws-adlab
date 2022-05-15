4ba517b03bfb203318f59123238751eb9864d514
cthare <colin@colinhare.com>
2022-05-14 20:48:22 -0400

    copy over

---
+++ main.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+### Network Setup ###
+resource "aws_vpc" "adlab_vpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+   Name = "adlab_vpc"
+  }
+ }
+ 
+resource "aws_internet_gateway" "adlab_igw" {
+  vpc_id = aws_vpc.adlab_vpc.id
+
+   tags = {
+     Name = "adlab_igw"
+   }
+}
+
+resource "aws_subnet" "adlab_sn" {
+  vpc_id =  aws_vpc.adlab_vpc.id
+  cidr_block = var.adlab_sn
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "adlab_sn"
+  }
+}
+
+resource "aws_default_route_table" "adlab_route_table" {
+  default_route_table_id = aws_vpc.adlab_vpc.default_route_table_id
+ 
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.adlab_igw.id
+    }
+  tags = {
+      Name = "adlab_route_table"
+    }
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+  vpc_id =  aws_vpc.adlab_vpc.id
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.adlab_win_sn
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.instance_connect_ips
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## EC2 Instance Setup ##
+
+resource "aws_network_interface" "adlab_dc01_nic" {
+  subnet_id   = aws_subnet.adlab_sn.id
+  private_ips = ["10.10.10.10"]
+  security_groups = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+# Domain Controller
+resource "aws_instance" "adlab_dc01" {
+  ami           = data.aws_ami.win2019.id
+  instance_type = "t2.micro"
+  key_name      = var.key_pair
+  
+  network_interface {
+    network_interface_id = aws_network_interface.adlab_dc01_nic.id
+    device_index         = 0
+  }
+
+  user_data     = "${file("userdata/dc01")}"
+
+  tags = {
+    Name = "adlab_dc01"
+  }
+}
+
+# Domain Member Server
+resource "aws_instance" "adlab_gen01" {
+  ami           = data.aws_ami.win2019.id
+  instance_type = "t2.micro"
+  subnet_id   = aws_subnet.adlab_sn.id
+  key_name      = var.key_pair
+  security_groups = [aws_security_group.web_sg.id]
+
+  user_data     = "${file("userdata/gen01")}"
+
+  tags = {
+    Name = "adlab_gen01"
+  }
+}
+
+# Domain Member Server
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.awsLinux.id
+  instance_type = "t2.micro"
+  subnet_id   = aws_subnet.adlab_sn.id
+  key_name      = var.key_pair
+  security_groups = [aws_security_group.web_sg.id]
+
+  user_data     = "${file("userdata/jenkins")}"
+
+  tags = {
+    Name = "jenkins"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,61 @@
+# General Setup
+# Set Key Pair
+variable "key_pair" {
+  description = "Key Pair Name"
+  type        = string
+  default     = "win-lab"
+}
+
+## Networking
+# Set VPC CIDR
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+# Set Subnet CIDR for creation
+variable "adlab_sn" {
+  description = "adlab Subnet CIDR"
+  type        = string
+  default     = "10.10.10.0/24"
+}
+
+# Set Subnet CIDR for SG
+variable "adlab_win_sn" {
+  description = "adlab Subnet CIDR"
+  type        = list(string)
+  default     = ["10.10.10.0/24"]
+}
+
+# Set to "My IP" for Services like RDP / SSH
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+variable "instance_connect_ips" {
+  description = "AWS instance connect IPs (US)"
+  type        = list(string)
+  default     = ["18.206.107.24/29", "3.16.146.0/29", "13.52.6.112/29", "18.237.140.160/29"]
+}
+
+# Instances
+# Get latest Windows Server 2019 AMI
+data "aws_ami" "win2019" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base*"]
+  }
+}
+
+# Get latest Amazon Linux AMI
+data "aws_ami" "awsLinux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs*"]
+  }
+}
